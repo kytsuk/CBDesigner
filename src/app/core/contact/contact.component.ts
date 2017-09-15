a579bd61c41b4a0797143814034cfa2a5c628a2b
kytsuk <kytsuk_ua@mail.ru>
2017-09-15 12:02:48 +0300

    contact form

--- src/app/core/contact/contact.component.ts
+++ src/app/core/contact/contact.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {Subscription} from "rxjs";
 import {Router} from "@angular/router";
 import {DataService} from "../../data.service";
-import {AuthService} from "../../auth/auth.service";
-
 
 @Component({
   selector: 'app-contact',
@@ -13,27 +10,24 @@ import {AuthService} from "../../auth/auth.service";
 })
 export class ContactComponent implements OnInit {
   commentForm: FormGroup;
-  id: number;
-  private sub: Subscription;
-  constructor(public  date: DataService, private router:Router, public auth: AuthService) {
-    }
+  constructor(private router: Router, public date: DataService) {}
 
   ngOnInit() {
     this.commentForm = new FormGroup({
       name: new FormControl('', Validators.required),
-      email: new FormControl('', Validators.required),
-      text: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.pattern("[a-zA-Z_]+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}")]),
+      text: new FormControl('', [Validators.required])
 
     });
   }
   onsubmit(commentForm: FormGroup) {
-  this.date.addComment([commentForm.value.name, commentForm.value.email, commentForm.value.text]);
-  this.goBack();
-  }
+    let date = new Date ();
+    let time = date.getFullYear() + '.' + (date.getMonth()+1) + '.' + date.getDate();
+    this.date.addComment([commentForm.value.name, commentForm.value.email, commentForm.value.text, time ]);
+    this.goBack()
 
+  }
   goBack() {
     this.router.navigate([''])
   }
-
-
 }
